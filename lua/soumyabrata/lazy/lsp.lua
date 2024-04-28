7e0815b2f9cbf98d94c0af820a87156002168230
SoumyabrataBanik <soumyabratabanik46@gmail.com>
2024-04-28 17:58:17 +0530

    Add tailwindcss lsp

--- lua/soumyabrata/lazy/lsp.lua
+++ lua/soumyabrata/lazy/lsp.lua
@@ -34,6 +34,7 @@ return {
                 "html",
                 "jsonls",
                 "prismals",
+                "tailwindcss",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
