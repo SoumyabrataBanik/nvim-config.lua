8af186cce2da21da448382a7e72645cbc6f1d751
SoumyabrataBanik <soumyabratabanik46@gmail.com>
2024-04-30 00:21:43 +0530

    Add bufferline using romgrk/barbar.nvim

--- lua/soumyabrata/remap.lua
+++ lua/soumyabrata/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+local opts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<leader><leader>", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>so", vim.cmd.so)
@@ -13,4 +14,16 @@ vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<leader>b", vim.cmd.NvimTreeFocus)
 
 -- Bufferline
-vim.keymap.set("n", "<leader>x", vim.cmd.bdelete)
+vim.keymap.set("n", "<C-x>", vim.cmd.BufferClose)
+vim.keymap.set('n', "<C-a>", vim.cmd.BufferPrevious)
+vim.keymap.set('n', '<C-d>', vim.cmd.BufferNext)
+vim.keymap.set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+vim.keymap.set('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+vim.keymap.set('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+vim.keymap.set('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+vim.keymap.set('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+vim.keymap.set('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+vim.keymap.set('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+vim.keymap.set('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+vim.keymap.set('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+vim.keymap.set('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
