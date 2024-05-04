71468637763d1c24740752d3a0876538eb108e4e
SoumyabrataBanik <soumyabratabanik46@gmail.com>
2024-05-04 13:13:12 +0530

    Add Moonfly colorscheme

--- lua/soumyabrata/lazy/colors.lua
+++ lua/soumyabrata/lazy/colors.lua
@@ -1,22 +1,16 @@
 function ColorMyPencils(color)
-    color = color or "cyberdream"
-    vim.cmd.colorscheme(color)
+	color = color or "moonfly"
+	vim.cmd.colorscheme(color)
 end
 
 return {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("cyberdream").setup({
-            -- Recommended - see "Configuring" below for more config options
-            transparent = true,
-            italic_comments = true,
-            hide_fillchars = true,
-            borderless_telescope = true,
-            terminal_colors = true,
-        })
-        vim.cmd("colorscheme cyberdream")-- set the colorscheme
-        ColorMyPencils()
-    end,
+	"bluz71/vim-moonfly-colors",
+	name = "moonfly",
+	lazy = false,
+	priority = 1000,
+
+	config = function()
+		vim.g.moonflyTransparent = true
+		ColorMyPencils()
+	end,
 }
